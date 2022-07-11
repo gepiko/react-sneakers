04bdfabca8fc54241d73b9ebbf0d78546ed31672
gepiko <aoriaira@gmail.com>
2022-07-11 15:32:32 +0300

    added shop card markup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
           <img src='/img/logo.png' alt='Logo' width={40} height={40} />
           <div>
             <h3 className='text-uppercase'>React Sneakers</h3>
-            <p>Магазин лучших кроссовок</p>
+            <p className='opacity-5'>Магазин лучших кроссовок</p>
           </div>
         </div>
         <ul className='d-flex'>
@@ -62,8 +62,46 @@ function App() {
         </ul>
       </header>
       <div className='content p-40'>
-        <h1>Все кроссовки</h1>
-        ...
+        <h1 className='mb-40'>Все кроссовки</h1>
+
+        <div className='d-flex'>
+          <div className='card'>
+            <img
+              src='/img/sneakers/1.jpg'
+              alt='Sneakers'
+              width={133}
+              height={112}
+            />
+            <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img src='/img/plus.svg' alt='Plus' width={11} height={11} />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img
+              src='/img/sneakers/2.jpg'
+              alt='Sneakers'
+              width={133}
+              height={112}
+            />
+            <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img src='/img/plus.svg' alt='Plus' width={11} height={11} />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
