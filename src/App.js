f7f1ab12960f89e4a3bc4f57c2751b125637bdca
gepiko <aoriaira@gmail.com>
2022-07-20 17:17:03 +0300

    adder routing, moved content into separate component, fixed markup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { Routes, Route } from 'react-router-dom'
+
 import axios from 'axios'
 
-import Card from './components/Card'
 import Drawer from './components/Drawer'
 import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
   const [items, setItems] = React.useState([])
@@ -42,12 +44,16 @@ function App() {
   }
 
   const onAddToFavorite = (obj) => {
-    axios.post(`https://62d2b2bf81cb1ecafa643d83.mockapi.io/favorites/`, obj)
+    axios.post(`https://62d2b2bf81cb1ecafa643d83.mockapi.io/favorites`, obj)
     setFavorites((prev) => [...prev, obj])
   }
 
   const onChangeSearchInput = (e) => {
     setSearchValue(e.target.value)
+  }
+
+  const Favorites = () => {
+    return <div>Favorites</div>
   }
 
   return (
@@ -62,53 +68,22 @@ function App() {
 
       <Header onCLickCart={() => setIsCartOpened(true)} />
 
-      <div className='content p-40'>
-        <div className='d-flex align-center justify-between mb-40'>
-          <h1>
-            {searchValue ? `Поиск по запросу ${searchValue}` : 'Все кроссовки'}
-          </h1>
-          <div className='searchBlock'>
-            <svg
-              width='16'
-              height='16'
-              viewBox='0 0 16 16'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                d='M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z'
-                stroke='#E4E4E4'
-                strokeWidth='2'
-                strokeLinecap='round'
-              />
-            </svg>
-            <input
-              onChange={onChangeSearchInput}
-              type='search'
-              placeholder='Поиск...'
-              value={searchValue}
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Home
+              items={items}
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              onChangeSearchInput={onChangeSearchInput}
+              onAddToFavorite={onAddToFavorite}
+              onAddToCart={onAddToCart}
             />
-          </div>
-        </div>
-
-        <div className='d-flex flex-wrap'>
-          {items
-            .filter((item) =>
-              item.title.toLowerCase().includes(searchValue.toLowerCase()),
-            )
-            .map((item) => (
-              <Card
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                imageUrl={item.imageUrl}
-                price={item.price}
-                onHeart={(obj) => onAddToFavorite(obj)}
-                onPlus={(obj) => onAddToCart(obj)}
-              />
-            ))}
-        </div>
-      </div>
+          }
+        />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
     </div>
   )
 }
